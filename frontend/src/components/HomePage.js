f9d74039f30b76e4a2b44e7bfd3f85811c880d87
Minh To <toanhminh0412@gmail.com>
2021-10-09 22:49:56 -0700

    Add music-controller project

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import Room from './Room'
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
+
+export default class HomePage extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            roomCode: null,
+        };
+        this.clearRoomCode = this.clearRoomCode.bind(this)
+    }
+
+    // without async we have to wait for everything in the function to happend before we can do anything else
+    async componentDidMount() {
+        fetch('/api/user-in-room').then((response) => response.json()).then((data) => {
+            this.setState({
+                roomCode : data.code
+            });
+        });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={Link}>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    clearRoomCode() {
+        this.setState({
+            roomCode: null,
+        });
+    }
+
+    render() {
+        return (<Router>
+            <Switch>
+                <Route exact path='/' render={() => {
+                    return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}></Redirect>) : this.renderHomePage()
+                }}>
+                </Route>
+                <Route path='/join' component={RoomJoinPage}></Route>
+                <Route path='/create' component={CreateRoomPage}></Route>
+                <Route 
+                    path='/room/:roomCode'
+                    render={(props) => {
+                        return <Room {...props} leaveRoomCallback={this.clearRoomCode}></Room>
+                    }}>
+                </Route>
+            </Switch>
+
+        </Router>);
+    }
+}
---
+++ spotify/models.py
@@ -0,0 +1,11 @@
+from types import TracebackType
+from django.db import models
+
+# Create your models here.
+class SpotifyToken(models.Model):
+    user = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    refresh_token = models.CharField(max_length=150, default="")
+    access_token = models. CharField(max_length=150)
+    expires_in = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
